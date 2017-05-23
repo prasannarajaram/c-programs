5b01c9ac9baf70985622721e8524892650f41d7e
Prasanna Rajaram <prasanna.uralsk@gmail.com>
2017-05-23 12:23:22 +0530

    removed syntax error in for loop

--- checkers.c
+++ checkers.c
@@ -3,12 +3,9 @@
 int x,y;
 int main(void)
 {
-  for (x=0; x<10; x++, printf("\n"))
+  for (x=0; x<=2; x++, printf("\n"))
     //    printf("\n");          
-  
-      
-    for (y=0;y<;1 y++)
-      printf("X ");
-  
+      for (y=0;y<=2; y++)
+      printf("X ");  
   return 0;
 }
